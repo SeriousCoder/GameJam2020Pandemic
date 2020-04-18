a27e6cff0a258fc98592e70cfa114b049b692ce9
nikita.tarasenko <nik.d.tarasenko@gmail.com>
2020-04-18 15:04:50 +0300

    --

---
+++ Assets/AIScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIScript : MonoBehaviour
+{
+    private Camera mainCamera;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/AIVision.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets
+{
+    sealed class AIVision
+    {
+        public float ActiveDis = 10;
+        public float ActiveAng = 35;
+
+        public bool VisionM(Transform player, Transform target)
+        {
+            return Dist(player, target) && Angle(player, target) && !CheckBloked(player, target);
+        }
+
+        private bool CheckBloked(Transform player, Transform target)
+        {
+            var hit2d = Physics2D.Linecast(player.position, target.position);
+
+            if (hit2d.transform == null) return true;
+            return hit2d.transform != target;
+        }
+
+        private bool Angle(Transform player, Transform target)
+        {
+            var angle = Vector3.Angle(player.forward, target.position - player.position);
+            return angle <= ActiveAng;
+        }
+
+        private bool Dist(Transform player, Transform target)
+        {
+            //var dist = Vector3.Distance(player.position, target.position); //todo оптимизация
+            Vector3 offset = target.position - player.position;
+            float sqrLen = offset.sqrMagnitude;
+            return sqrLen <= ActiveDis * ActiveDis;
+        }
+    }
+}
