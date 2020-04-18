5983f1128f0d3e7e8d7ae5fff727b218bfa6cac3
Pushkan <pushkan13@gmail.com>
2020-04-18 15:02:39 +0300

    Добавил PlayerController.cs и UIController.cs

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    private Text _textHP;
+    private Text _textCharge;
+    private PlayerController _playerController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerController = GetComponent<PlayerController>();
+        var _panel = transform.Find("Canvas").transform.Find("Panel");
+        _textHP = _panel.Find("TextHP").GetComponentInChildren<Text>();
+        _textCharge = _panel.Find("TextCharge").GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _textHP.text = "HP: " + _playerController.GetCurrentHP();
+        _textCharge.text = "Charge: " + _playerController.GetCurrentCharge();
+    }
+}
