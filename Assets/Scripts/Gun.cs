c413b57e9895856e99e5bfe0f9f3e97147e2c7c6
Pushkan <pushkan13@gmail.com>
2020-04-19 12:41:00 +0300

    +Gun, Bullet, new tags

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private Transform _barrel;
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private float _cooldown = 0.3f;
+    private float _currentCooldown;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (_currentCooldown <= _cooldown)
+            _currentCooldown += Time.deltaTime;
+    }
+
+    public void Fire()
+    {
+        if (_currentCooldown > _cooldown)
+        {
+            Instantiate(_bullet, _barrel);
+            _currentCooldown = 0;
+        }
+    }
+}
