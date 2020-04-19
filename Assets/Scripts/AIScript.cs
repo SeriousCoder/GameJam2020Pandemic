f76f0f19ab49c26704b0747622bd898890c8fe32
nikita.tarasenko <nik.d.tarasenko@gmail.com>
2020-04-19 16:01:51 +0300

    Добавлены состояния AI Бот теперь смотрит куда идет)

--- Assets/Scripts/AIScript.cs
+++ Assets/Scripts/AIScript.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Assets;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class AIScript : MonoBehaviour
 {
@@ -13,9 +15,25 @@ public class AIScript : MonoBehaviour
     private AIVision vision;
     private GameObject player;
 
+
+    [SerializeField] private float speed;
+    [SerializeField] private Transform[] moveSpots;
+    [SerializeField] private int randomSpot;
+
+    [SerializeField] private float defWaitTime = 5.0f;
+    [SerializeField] private float waitTime;
+    
+    [SerializeField] private float defChaseTime = 5.0f;
+    private float chaseTime;
+    [SerializeField] private BotState state;
+    private Vector3 lastPlayerPos;
+
     enum BotState
     {
-        idle
+        Idle,
+        Patrol,
+        Chase,
+        Alarm
     }
 
     void Start()
@@ -29,21 +47,94 @@ public class AIScript : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+
+        randomSpot = Random.Range(0, moveSpots.Length);
+        state = BotState.Idle;
     }
 
     void Update()
     {
+        switch (state)
+        {
+            case BotState.Idle:
+                state = BotState.Patrol;
+                randomSpot = Random.Range(0, moveSpots.Length);
+                SetDestinationToPoint(moveSpots[randomSpot]);
+                waitTime = defWaitTime;
+                break;
+            case BotState.Patrol:
+                //Debug.Log(Vector2.Distance(moveSpots[randomSpot].position, transform.position));
+                if (Vector2.Distance(moveSpots[randomSpot].position, transform.position) < 0.1f)
+                {
+                    if (waitTime <= 0)
+                    {
+                        randomSpot = Random.Range(0, moveSpots.Length);
+                        SetDestinationToPoint(moveSpots[randomSpot]);
+                        waitTime = defWaitTime;
+                    }
+                    else
+                    {
+                        waitTime -= Time.deltaTime;
+                    }
+                }
+                SetDestinationToPoint(moveSpots[randomSpot]);
+
+                break;
+            case BotState.Alarm:
+                //Debug.Log(Vector2.Distance(lastPlayerPos, transform.position));
+                if (Vector2.Distance(lastPlayerPos, transform.position) < 0.1f)
+                {
+                    state = BotState.Chase;
+                    chaseTime = defChaseTime;
+                }
+                break;
+            case BotState.Chase:
+                if (chaseTime > 0.0)
+                {
+                    chaseTime -= Time.deltaTime;
+                }
+                else
+                {
+                    state = BotState.Idle;
+                }
+                break;
+        }
+
         if (vision.VisionM(player.transform, transform))
         {
             Vector2 direction = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
             transform.up = direction;
             agent.SetDestination(player.transform.position);
+
+            lastPlayerPos = player.transform.position;
+            state = BotState.Alarm;
+
+            SetDestinationToPoint(player.transform);
         }
 
-        DrawRadiusObzora();
+        //for (int i = 0; i < agent.path.corners.Length - 1; i++)
+        //{
+        //    Debug.DrawLine(agent.path.corners[i], agent.path.corners[i + 1], Color.red);
+        //}
+        if (agent.path.corners.Length > 1) LookAtPoint(agent.path.corners[1]);
+
+        DrawVisionRadius();
     }
 
-    private void DrawRadiusObzora()
+    private void SetDestinationToPoint(Transform target)
+    {
+        Vector2 direction = new Vector2(target.position.x - transform.position.x, target.position.y - transform.position.y);
+        transform.up = direction;
+        agent.SetDestination(target.position);
+    }
+
+    private void LookAtPoint(Vector3 point)
+    {
+        Vector2 direction = new Vector2(point.x - transform.position.x, point.y - transform.position.y);
+        transform.up = direction;
+    }
+
+    private void DrawVisionRadius()
     {
         float j = 0;
         int rays = 6;
